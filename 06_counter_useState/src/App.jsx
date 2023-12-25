501e30585a00d1d8e352d03d51cfd0c851dea5a7
AadityaRajPaudel <aadityapaudel834@gmail.com>
2023-12-25 21:25:49 +0545

    forms in React

--- 06_counter_useState/src/App.jsx
+++ 06_counter_useState/src/App.jsx
@@ -3,21 +3,122 @@ import './app.css';
 
 export default function App() {
 
-  let [count, setCount] = React.useState(0);
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comment: "",
+    isHappy: true,
+    employee: "",
+    car: ""
+  })
 
-  function addCount() {
-    setCount(prevCount => prevCount+1);
+  const id = React.useId(); // generates unique id that can be used in htmlFor and id attributes in label and input component
+
+  function handleChange(event) {
+    const {name, value, type, checked} = event.target ;
+    setFormData(prevFormData =>{
+      return {
+        ...prevFormData,
+        [name]: type==="checkbox"?checked:value
+      }
+    })
   }
 
-  function subCount() {
-    setCount(prevCount => prevCount-1);
+  function handleSubmit(event) {
+    // prevents the page from re-rendering after submit button is clicked
+    event.preventDefault(); 
+    // submitToAPI(formData); > some function that sends data to API
+    console.log(formData)
   }
 
   return (
-    <div className="app-container">
-      <button className="button" onClick={addCount}>+</button>
-      <div className = "app-count">{count}</div>
-      <button className="button" onClick={subCount}>-</button>
-    </div>
+    <form onSubmit={handleSubmit}>
+
+      <label htmlFor={id+"-firstname"}></label>
+      <input 
+        id={id+"-firstname"}
+        type="text" 
+        placeholder="First Name" 
+        name="firstName" 
+        value={formData.firstName} 
+        onChange={handleChange}
+      />
+
+      <label htmlFor={id+"-lastname"}></label>
+      <input 
+        id={id+"-lastname"}
+        type="text" 
+        placeholder="Last Name" 
+        name="lastName" 
+        value={formData.lastName} 
+        onChange={handleChange}
+      />
+
+      <label htmlFor={id+"-email"}></label>
+      <input 
+        id={id+"-email"}
+        type="email" 
+        placeholder="Email" 
+        name="email" 
+        value={formData.email} 
+        onChange={handleChange}
+      />
+
+      <label htmlFor={id+"-comment"}></label>
+      <textarea
+        id={id+"-comment"}
+        placeholder="Enter some comments"
+        name="comment"
+        value={formData.comment}
+        onChange={handleChange}
+      ></textarea>
+
+      <label htmlFor={id+"-isHappy"}></label>
+      <input 
+        type="checkbox" 
+        name="isHappy" 
+        checked = {formData.isHappy}
+        onChange={handleChange}
+        id="isHappy"
+      />
+      <label htmlFor="isHappy">Are you Happy?</label>
+
+      <input 
+        type="radio" 
+        name="employee" 
+        value="fullTime"
+        checked = {formData.employee === "fullTime"}
+        onChange={handleChange}
+        id="fulltime"
+      />
+      <label htmlFor="fulltime">Full Time</label>
+
+      <input 
+        type="radio" 
+        name="employee" 
+        value="partTime"
+        checked = {formData.employee === "partTime"}
+        onChange={handleChange}
+        id="parttime"
+      />
+      <label htmlFor="parttime">Part-Time</label>
+
+      <label htmlFor="cars">Select a car:</label>
+      <select 
+        name="car" 
+        value={formData.car} 
+        onChange={handleChange}
+        id="cars" 
+      >
+        <option value="">--Choose--</option>
+        <option value="BMW">BMW</option>
+        <option value="Hyundai">Hyundai</option>
+      </select>
+
+      {/* Any button inside form tags will automatically act as type='submit' */}
+      <button>Submit</button>
+
+    </form>
   );
 }
